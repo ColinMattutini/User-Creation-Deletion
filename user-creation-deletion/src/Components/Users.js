f4d05fbe1879609e91e9c2924d81858479a21087
Colin <cmmatt14@gmail.com>
2022-08-20 22:06:56 -0400

    Added new user form and new user component

--- user-creation-deletion/src/Components/Users.js
+++ user-creation-deletion/src/Components/Users.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Users.css';
 import Card from './UI/Card.js';
 
 const Users = (props) => {
 
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
+    const nameChangeHandler = (event) =>{
+        setEnteredName(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+
+        setEnteredAge(event.target.value);
+    }
+
+
+    const submitHandler = (event) =>{
+        event.preventDefault();
+
+        const userData = {
+            name: enteredName,
+            age: enteredAge
+        };
+
+        props.onSaveUserData(userData);
+        setEnteredName('');
+        setEnteredAge('');
+
+    };
+
     return(
         <Card>
+            <form onSubmit={submitHandler}>
         <div className='new-user'>
-        <form>
+        
         <div className='new-user__controls'>
             <div className='new-user__control'>
                 <label>Name</label>
-                <input type='text'/>
+                <input type='text' value={enteredName} onChange={nameChangeHandler}/>
             </div>
             <div className='new-user__control'>
                 <label>Age</label>
-                <input type='number'/>
+                <input type='number' value={enteredAge} onChange={ageChangeHandler}/>
             </div>
-            <button type='button'>Submit</button>
+            <button type='submit'>Submit</button>
+        </div>
         </div>
         </form>
-        </div>
         </Card>
 
     )
